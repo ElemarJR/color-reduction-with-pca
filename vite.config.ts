f004a4143e63f9a119d2acad22cb90307aebe3c7
Douglas Picolotto <douglas.picolotto@gmail.com>
2024-11-27 09:47:31 -0300

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({ 
+export default defineConfig({
+  base: '/color-reduction-with-pca/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
